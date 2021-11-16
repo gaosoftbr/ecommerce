245579cdb8f6c0f193590a3ccb03fe86770dac53
gaosoftbr <suporte@gaosoft.com.br>
2021-11-16 11:38:42 -0300

    initial project

---
+++ app/api/GAO/Core/Application.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace GAO\Core;
+
+class Application{
+
+    public function run(){
+        echo "sou class application";
+    }
+
+}
+
---
+++ app/app.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = new \GAO\Core\Application(dirname(dirname(__DIR__)));
+
+return $app;
